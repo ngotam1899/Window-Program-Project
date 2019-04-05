aeb11307f5c688b9687e4b5b58e4668b3e75420a
Ngo Tam <ngotam1899@gmail.com>
2019-04-05 12:54:35 +0700

    First Commit - Shotting game

---
+++ DoAn/DoAn/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace DoAn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pillar1 = new System.Windows.Forms.PictureBox();
+            this.pillar2 = new System.Windows.Forms.PictureBox();
+            this.ufo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::DoAn.Properties.Resources.Halicopter;
+            this.player.Location = new System.Drawing.Point(73, 219);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 54);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // pillar1
+            // 
+            this.pillar1.Image = global::DoAn.Properties.Resources.pillar;
+            this.pillar1.Location = new System.Drawing.Point(338, -6);
+            this.pillar1.Name = "pillar1";
+            this.pillar1.Size = new System.Drawing.Size(75, 185);
+            this.pillar1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pillar1.TabIndex = 1;
+            this.pillar1.TabStop = false;
+            // 
+            // pillar2
+            // 
+            this.pillar2.Image = global::DoAn.Properties.Resources.pillar;
+            this.pillar2.Location = new System.Drawing.Point(667, 253);
+            this.pillar2.Name = "pillar2";
+            this.pillar2.Size = new System.Drawing.Size(75, 185);
+            this.pillar2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pillar2.TabIndex = 2;
+            this.pillar2.TabStop = false;
+            // 
+            // ufo
+            // 
+            this.ufo.BackColor = System.Drawing.Color.Transparent;
+            this.ufo.Image = global::DoAn.Properties.Resources.alien1;
+            this.ufo.Location = new System.Drawing.Point(804, 120);
+            this.ufo.Name = "ufo";
+            this.ufo.Size = new System.Drawing.Size(68, 72);
+            this.ufo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.ufo.TabIndex = 3;
+            this.ufo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DoAn.Properties.Resources.city;
+            this.ClientSize = new System.Drawing.Size(929, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ufo);
+            this.Controls.Add(this.pillar2);
+            this.Controls.Add(this.pillar1);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pillar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ufo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pillar1;
+        private System.Windows.Forms.PictureBox pillar2;
+        private System.Windows.Forms.PictureBox ufo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ DoAn/DoAn/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAn
+{
+    public partial class Form1 : Form
+    {
+        //enter the variables
+        bool goup; //cho phép người chơi đi lên
+        bool godown; //cho phép người chơi đi xuống
+        bool shot = false;//kiểm tra xem người chơi có bắn phát nào chưa
+        int score = 0;// số điểm người chơi
+        int speed = 5;//tốc độ ufo
+        Random rand = new Random();
+        int playerSpeed = 7;//tốc độ di chuyển của người chơi
+        int index;//thay đổi hình ufo
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                //nếu người chơi bấm lên thay đổi biến thành true
+                goup = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                //nếu người chơi bấm xuống thay đổi biến thành true
+                godown = true;
+            }
+            if(e.KeyCode==Keys.Space && shot == false)
+            {
+                //nếu người chơi bấm space và biến shot là false. Sau đó chạy hàm bullet và gián lại shot = true
+                MakeBullet();
+                shot = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                //nếu người chơi bấm lên thay đổi biến thành false
+                goup = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                //nếu người chơi bấm xuống thay đổi biến thành false
+                godown = false;
+            }
+            if (shot==true)
+            {
+                //nếu biến shot = true thì đổi lại false để người chơi bắn lại
+                shot = false;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //dịch chuyển bức tường về trái màn hình
+            pillar1.Left -= speed;
+            pillar2.Left -= speed;
+            //dịch chuyển ufo về trái
+            ufo.Left -= speed;
+            //Điểm số của người chơi
+            label1.Text = "Score: " + score;
+            if (goup)
+            {
+                // nếu goup=true di chuyển người chơi đi lên
+                player.Top -= playerSpeed;
+            }
+            if (godown)
+            {
+                //nếu godown=true di chuyển người chơi đi xuống
+                player.Top += playerSpeed;
+            }
+            if (pillar1.Left < -150)
+            {
+                //nếu bức tường đi khỏi màn hình, dịch chuyển bức tường sang phần bên phải (dịch chuyển từ phải sang trái)
+                pillar1.Left = 900;
+            }
+            if (pillar2.Left < -150)
+            {
+                //nếu bức tường đi khỏi màn hình, dịch chuyển bức tường sang phần bên phải (dịch chuyển từ phải sang trái)
+                pillar2.Left = 1000;
+            }
+            if(ufo.Left<-5 || player.Bounds.IntersectsWith(ufo.Bounds) || player.Bounds.IntersectsWith(pillar1.Bounds) || player.Bounds.IntersectsWith(pillar2.Bounds))
+            {
+                gameTimer.Stop();
+                //hiện ra điểm số cuối cùng của người chơi
+                MessageBox.Show("Game over: " + score);
+            }
+            foreach(Control X in this.Controls)
+            {
+                //nếu X là hình đầu đạn
+                if(X is PictureBox && X.Tag == "bullet")
+                {
+                    //dịch chuyển X về pahi3 màn hình
+                    X.Left +=15;
+                    //nếu x đi ra khỏi màn hình
+                    if (X.Left > 900)
+                    {
+                        this.Controls.Remove(X);
+                        X.Dispose();
+                    }
+                    //nếu X đụng phải UF0
+                    if (X.Bounds.IntersectsWith(ufo.Bounds))
+                    {
+                        //cộng 1 điểm
+                        score += 1;
+                        //dịch chuyển đạn ra khỏi màn hình
+                        this.Controls.Remove(X);
+                        X.Dispose();
+                        //dịch chuyển UF0
+                        ufo.Left = 1000;
+                        ufo.Top = rand.Next(5, 330) - ufo.Height;
+                        ChangeUFO();
+                    }
+                }
+            }
+        }
+        private void ChangeUFO()
+        {
+            index += 1;//thay đổi hình UF0
+            if (index > 3)
+            {
+                //nếu đổi hết 3 hình, trở lại hình đầu tiên
+                index = 1;
+            }
+            switch (index)
+            {
+                //hình 1
+                case 1:
+                    ufo.Image = Properties.Resources.alien1;
+                    break;
+                //hình 2
+                case 2:
+                    ufo.Image = Properties.Resources.alien2;
+                    break;
+                //hình 3
+                case 3:
+                    ufo.Image = Properties.Resources.alien3;
+                    break;
+            }
+        }
+        private void MakeBullet()
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.BackColor = System.Drawing.Color.DarkOrange;
+            bullet.Height = 5;
+            bullet.Width = 10;
+            bullet.Left = player.Left + player.Width; // bay trước mặt
+            bullet.Top = player.Top + player.Height / 2; //bay chính giữa
+            bullet.Tag = "bullet";
+            this.Controls.Add(bullet);
+            //thêm hình bullet
+        }
+    }
+}
